b2d9e55bd8aa6576e72ec26e7f18087b76d5dde4
appuser <appuser@journey2-754f4b4fb-c2vrf>
2022-02-25 15:23:35 +0000

    starter project

---
+++ phonebook.py
@@ -0,0 +1,27 @@
+# Do not modify this list
+phone_list = ["98-777-653", "98-742-644", "34-855-326", "34-25-629", "34-489-115", "72-999-563", "9-321-459",
+              "72-654-121", "72-4-694", "72-255-313", "98-111-323", "98-433-14", "34-577-342", "98-323-000",
+              "98-202-666", "34-5435-454", "34-515-633"]
+
+ugly_phone_list = ["98-333-111", "12--323-566", "123-34-221", "99-948-321", "-12-123-346",
+                     "894-58438-543", "85-234-222",
+                     "12-333-444-", "99-888--433", "15-465-876", "98-555-22", "-3-355-333", "3--344-53", "--2--45---",
+                     "11-111-222", "12#22$34$222", "98 223 555"]
+
+
+# "These are the valid phone numbers in your phonebook:"
+# "and these are the wrong ones:"
+
+
+# "The area codes:"
+# "and the numbers without the area codes:"
+
+
+# "The unique area codes:"
+
+
+# "You have X numbers from area 98, Y numbers from area 34, and Z numbers from area 72."
+
+
+# "These are the pretty phone numbers:"
+# "and these are the ugly ones:"
